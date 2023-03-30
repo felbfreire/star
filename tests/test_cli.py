7775537e7cdd420733a7d162de92ebda276e3cf2
felbfreire <felbfreire@proton.me>
2023-03-30 15:07:30 -0300

    update tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -10,3 +10,14 @@ class TestCli():
         with Client(base_url=url) as client:
             status_code = client.get('/').status_code
             assert status_code == 200
+
+    def test_homepage_method_post_should_return_405(self):
+        with Client(base_url=url) as client:
+            status_code = client.post('/').status_code
+            assert status_code == 405
+
+    def test_userpage_greets_user(self):
+        with Client(base_url=url) as client:
+            user = 'Alfred'
+            content = client.get('/{}'.format(user)).content
+            assert content == b'Hello Alfred'
