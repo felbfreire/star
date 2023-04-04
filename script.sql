91a06f836bc5a3f241cd3f4158694c14e94ae81e
felbfreire <felbfreire@proton.me>
2023-04-04 11:37:20 -0300

    add basic script.sql

---
+++ script.sql
@@ -0,0 +1,6 @@
+drop table if exists star;
+create table if not exists stars(id integer primary key, star_name char(30));
+--insert into stars(star_name) values ('Alcyone');
+--insert into stars(star_name) values ('Atlas');
+--insert into stars(star_name) values ('Electra');
+--insert into stars(star_name) values ('Pleione');
