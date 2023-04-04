be6ac29565a42f5b09abd08eeb9d1f5801f39525
felbfreire <felbfreire@proton.me>
2023-04-04 17:26:05 -0300

    replace for init_db

--- star/db.py
+++ star/db.py
@@ -1,18 +1,20 @@
 from databases import Database
 
 
-DB = '../star.db'
+DB_FILE = '../star.db'
 SCRIPT = '../script.sql'
+DATABASE = 'sqlite+aiosqlite:///{}'
 
 async def connect_database():
-    database = Database('sqlite+aiosqlite:///{}'.format(DB))
+    database = Database(DATABASE.format(DB_FILE))
     await database.connect()
     return database
 
 async def init_db():
-    database = await connect_database()
-    with open(SCRIPT, 'r') as file:
-        for line in file:
-            if line:
-                await database.execute(line)
+    async with Database(DATABASE.format(DB_FILE)) as database:
+        with open(SCRIPT, 'r') as file:
+            for line in file:
+                if line:
+                    await database.execute(line)
+            await database.disconnect()
 
