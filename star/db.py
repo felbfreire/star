e8381c8c269ecda462a2f0356634cb27488f68f3
Felipe Freire <felbfreire@proton.me>
2023-04-06 18:09:35 -0300

    add config from .env

--- star/db.py
+++ star/db.py
@@ -1,17 +1,21 @@
+from starlette.config import Config
+
 from databases import Database
 
 
+config = Config('.env')
 DB_FILE = '../star.db'
 SCRIPT = '../script.sql'
 DATABASE = 'sqlite+aiosqlite:///{}'
+DATABASE_URL = config('DATABASE_URL')
 
 async def connect_database():
-    database = Database(DATABASE.format(DB_FILE))
+    database = Database(DATABASE_URL)
     await database.connect()
     return database
 
 async def init_db():
-    async with Database(DATABASE.format(DB_FILE)) as database:
+    async with Database(DATABASE_URL) as database:
         with open(SCRIPT, 'r') as file:
             for line in file:
                 if line:
